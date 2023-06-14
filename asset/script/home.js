78af78ea9ecb977a85402f090c59797bde3aa444
Ismayil Hasanzade <126759096+ismayil-hasanzade@users.noreply.github.com>
2023-06-14 18:47:06 +0400

    Slick slide

--- asset/script/home.js
+++ asset/script/home.js
@@ -5,6 +5,7 @@ const xbtn = document.querySelector(".pageNavIcon");
 const searchIcon = document.querySelector(".searchIcon");
 const serch = document.querySelector(".search");
 const searchbutton = document.querySelector(".searchbutton");
+const cards = document.querySelector(".cards");
 navbtn.addEventListener("click", () => {
   navbtn.style.cssText = "display:none";
   secretmenu.classList.toggle("change");
@@ -22,4 +23,43 @@ searchIcon.addEventListener("click", () => {
 });
 searchbutton.addEventListener("click", () => {
   serch.style.cssText = "display:flex";
+});
+// Slick CODE
+$(".cards").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+       
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
