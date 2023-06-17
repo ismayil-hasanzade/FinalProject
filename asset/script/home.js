00bb4da49413a0016026fa45121752e25401fb43
Ismayil Hasanzade <126759096+ismayil-hasanzade@users.noreply.github.com>
2023-06-17 21:49:59 +0400

    All pages complated

--- asset/script/home.js
+++ asset/script/home.js
@@ -1,29 +1,4 @@
-const nav = document.getElementById("nav");
-const navbtn = document.querySelector(".fa-bars");
-const secretmenu = document.querySelector(".secretmenu");
-const xbtn = document.querySelector(".pageNavIcon");
-const searchIcon = document.querySelector(".searchIcon");
-const serch = document.querySelector(".search");
-const searchbutton = document.querySelector(".searchbutton");
 const cards = document.querySelector(".cards");
-navbtn.addEventListener("click", () => {
-  navbtn.style.cssText = "display:none";
-  secretmenu.classList.toggle("change");
-  xbtn.style.cssText = `
-  display:block;
-  `;
-});
-xbtn.addEventListener("click", () => {
-  secretmenu.classList.toggle("change");
-  navbtn.style.cssText = "display:block";
-  xbtn.style.cssText = "display:none";
-});
-searchIcon.addEventListener("click", () => {
-  serch.style.cssText = "display:none";
-});
-searchbutton.addEventListener("click", () => {
-  serch.style.cssText = "display:flex";
-});
 // Slick CODE
 $(".cards").slick({
   dots: true,
